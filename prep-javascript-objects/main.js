582c3b57b6f8b5115960b7221ec2e89da6ca2683
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-06-14 02:57:52 -0700

    :(

--- prep-javascript-objects/main.js
+++
@@ -1,9 +0,0 @@
-var person = {
-  firstName: "Humberto",
-  lastName: "Navarro",
-  hobby: "Guitar"
-};
-console.log(person.firstName + " " + person.lastName);
-person.job = "Stocking Associate";
-person.previousJob = "Sortation Associate";
-console.log(person);
