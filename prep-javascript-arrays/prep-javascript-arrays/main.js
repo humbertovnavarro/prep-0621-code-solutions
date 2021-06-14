6b9f4d51a6a9c1bef5ffff34e5fff31a1f43519f
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-06-14 03:06:41 -0700

    finished

---
+++ prep-javascript-arrays/prep-javascript-arrays/main.js
@@ -0,0 +1,16 @@
+var languages =
+[
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+]
+
+console.log(languages);
+
+firstElement = languages[0];
+thirdElement = languages[3];
+length = languages.length;
+lastElement = languages[languages.length - 1];
