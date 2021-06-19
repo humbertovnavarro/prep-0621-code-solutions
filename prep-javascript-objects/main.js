65af37bff5f191a73b4c956b009005808378e023
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-06-19 13:10:58 -0700

    manual merge

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,9 @@
+var person = {
+  firstName: "Humberto",
+  lastName: "Navarro",
+  hobby: "Guitar"
+};
+console.log(person.firstName + " " + person.lastName);
+person.job = "Stocking Associate";
+person.previousJob = "Sortation Associate";
+console.log(person);
